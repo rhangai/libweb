ee734301ec2958438be400f48daf3d21af2a8727
renanhangai <hangai.renan@gmail.com>
2017-03-18 00:51:23 -0300

    Updated validator test

--- test/ValidatorTest.php
+++ test/ValidatorTest.php
@@ -3,14 +3,21 @@ use LibWeb\Validator as v;
 
 class ValidatorTest extends PHPUnit\Framework\TestCase
 {
-	public function testValidate() {
-		$rules = array(
-			"email" => v::email()
-		);
 
-		$data = array(
-		);
-
-		v::validateObj( $data, $rules );
+	/**
+	 *  @dataProvider is_numeric_provider
+	 */
+	public function test_is_numeric( $expected, $v, $separator = "." ) {
+		$this->assertEquals( $expected, v::validate( $v, v::is_numeric( $separator ) ) );
+	}
+	public function is_numeric_provider() {
+		return [
+			[ "0", 0 ],
+			[ "100", 100 ],
+			[ "100", "100" ],
+			[ "100.01", 100.01 ],
+			[ "100.01", "100.01" ],
+			[ "100.01", "100,01", ",." ],
+		];
 	}
 }
