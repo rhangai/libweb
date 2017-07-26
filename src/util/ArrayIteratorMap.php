a26608f4e9a3ebc601b0670b15b078953607de74
renanhangai <hangai.renan@gmail.com>
2017-07-26 14:19:07 -0300

    Added map iterator for strings to single field

--- src/util/ArrayIteratorMap.php
+++ src/util/ArrayIteratorMap.php
@@ -5,7 +5,12 @@ class ArrayIteratorMap extends ArrayIteratorBase {
 	private $callback_;
 	public function __construct( \Iterator $innerIterator, $callback ) {
 		parent::__construct( $innerIterator );
-		$this->callback_ = $callback;
+		if ( is_string( $callback ) )
+			$this->callback_ = function( $value ) use ( $callback ) { return $value->{$callback}; };
+		else if ( is_callable( $callback ) )
+			$this->callback_ = $callback;
+		else
+			throw new \InvalidArgumentException( "Callback for map must be a string or a callable" );
 	}
 	protected function update( $it, $status ) {
 		$status->valid = $it->valid();
