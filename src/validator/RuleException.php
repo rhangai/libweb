2d3d0798d185f4b160349f94abb451cf0163e428
renanhangai <hangai.renan@gmail.com>
2017-11-09 13:40:20 -0200

    Added method to create exception with passed value

--- src/validator/RuleException.php
+++ src/validator/RuleException.php
@@ -5,5 +5,10 @@ namespace LibWeb\validator;
  * Simple Rule Exception
  */
 class RuleException extends \Exception {
+
+	public static function createWithValue( $message, $value ) {
+		$message .= " Passed ".gettype( $value ).": ".$value;
+		return new static( $message );
+	}
 }
 
